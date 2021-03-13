c4bdf86c2d2821f86805479042efb23c5dde2fb7
danrakh <raf.kepa@gmail.com>
2021-03-13 15:37:31 +0100

    rewriting lost code

--- map-the-debris.js
+++ map-the-debris.js
@@ -1,7 +1,18 @@
 function orbitalPeriod(arr) {
-    var GM = 398600.4418;
-    var earthRadius = 6367.4447;
-    return arr;
+    let myArr = [];
+    const GM = 398600.4418;
+    const earthRadius = 6367.4447;
+
+    arr.forEach(element => {
+      let orbitalPeriod = Math.round(2 * Math.PI * (Math.sqrt(Math.pow(element.avgAlt+earthRadius, 3)/GM)));
+      let myObj = {
+        name : element.name,
+        orbitalPeriod : orbitalPeriod
+      };
+      myArr.push(myObj);
+    });
+
+    return myArr;
   }
   
-  orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+  orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]);
